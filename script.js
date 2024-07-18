fd523c6d39a32493dfb366e1dee6d52853e0196d
Jigs <114229562+Jigs-stack@users.noreply.github.com>
2024-07-18 10:56:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var hiddenButton = $('#action-button').hide();
+var hiddenAnotherMessage = $('.another-message').hide();
+$(document).ready(function() {
+  const $messageElement = $("#message");
+  const message = "Hello langggingggg, this is for you!";
+  let index = 0;
+  $("#message").css('font-size', '60px');
+  function typeWriter() {
+      if (index < message.length) {
+          $messageElement.text($messageElement.text() + message.charAt(index));
+          index++;
+          setTimeout(typeWriter, 100);
+          
+      }
+      else {
+        hiddenButton.show();
+      }
+  }
+
+  typeWriter();
+});
+var hiddenFlowers = $("#body").hide();
+
+$("#action-button").click(function() {
+  hiddenFlowers.show();
+  var hiddenContainer = $('.wrapper').hide();
+  const c = setTimeout(() => {
+    $("#body").removeClass("not-loaded");
+    clearTimeout(c);
+    
+  }, 1000); 
+
+  setTimeout(function() {
+    hiddenAnotherMessage.show();
+  }, 6000);
+});
+
+  
+
